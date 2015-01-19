8eaa00a413ce032767704af140f0a9341bf842f6
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2015-01-19 17:24:59 -0500

    update config.js to use env variables instead of hard coding them in

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: process.env.consumer_key || 'ConsumerKey',
+  consumer_secret: process.env.consumer_secret || 'ConsumerSecret',
+  token: process.env.token || 'Token',
+  token_secret: process.env.token_secret || 'TokenSecret'
+};
